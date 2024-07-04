17f9bdc3223153c2a27f744f28e589cde7d64881
zibaee-2021 <shahinjzibaee@hotmail.co.uk>
2024-07-04 14:13:59 +0100

    Read specified fasta sequence(s) locally or from Uniprot API. If no ids specified, read all locally saved FASTA files.

--- src/aa_to_atoms.py
+++ src/aa_to_atoms.py
@@ -1,48 +1,63 @@
 from Bio.PDB import PDBParser
-
-# Define the atomic composition of each amino acid
-amino_acid_atoms = {
-    'A': ['N', 'CA', 'C', 'O', 'CB'],
-    'C': ['N', 'CA', 'C', 'O', 'CB', 'SG'],
-    'D': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'OD1', 'OD2'],
-    'E': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD', 'OE1', 'OE2'],
-    'F': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD1', 'CD2', 'CE1', 'CE2', 'CZ'],
-    'G': ['N', 'CA', 'C', 'O'],
-    'H': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'ND1', 'CD2', 'CE1', 'NE2'],
-    'I': ['N', 'CA', 'C', 'O', 'CB', 'CG1', 'CG2', 'CD1'],
-    'K': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD', 'CE', 'NZ'],
-    'L': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD1', 'CD2'],
-    'M': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'SD', 'CE'],
-    'N': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'OD1', 'ND2'],
-    'P': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD'],
-    'Q': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD', 'OE1', 'NE2'],
-    'R': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD', 'NE', 'CZ', 'NH1', 'NH2'],
-    'S': ['N', 'CA', 'C', 'O', 'CB', 'OG'],
-    'T': ['N', 'CA', 'C', 'O', 'CB', 'OG1', 'CG2'],
-    'V': ['N', 'CA', 'C', 'O', 'CB', 'CG1', 'CG2'],
-    'W': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD1', 'CD2', 'NE1', 'CE2', 'CE3', 'CZ2', 'CZ3', 'CH2'],
-    'Y': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD1', 'CD2', 'CE1', 'CE2', 'CZ', 'OH']
-}
+import json
+import fasta_reader as reader
 
 
-# Function to translate an amino acid sequence into its corresponding atomic sequence
-def translate_to_atoms(sequence):
+def translate_to_atoms(aa_fasta_sequence: str) -> str:
+    """
+    Translate the given amino acid sequence to its corresponding atomic sequence using the PDB atom naming convention.
+    :param aa_fasta_sequence: Amino acid sequence using 1-letter format.
+    :return: The atomic sequence corresponding to the given amino acid sequence.
+    """
     atomic_sequence = []
-    for residue in sequence:
-        if residue in amino_acid_atoms:
-            atomic_sequence.extend(amino_acid_atoms[residue])
+    for aa in aa_fasta_sequence:
+        if aa in amino_acid_atoms:
+            atomic_sequence.extend(amino_acid_atoms[aa])
         else:
-            raise ValueError(f"Unknown amino acid: {residue}")
-    return atomic_sequence
+            raise ValueError(f'{aa} is not one of the 20 amino acids (using 1-letter format)')
+    return ''.join(atomic_sequence)
 
 
 if __name__ == '__main__':
 
-    # Example amino acid sequence
-    amino_acid_sequence = 'ACDEFGHIKLMNPQRSTVWY'
+    # aa_atoms = {
+    #     'A': ['N', 'CA', 'C', 'O', 'CB'],
+    #     'C': ['N', 'CA', 'C', 'O', 'CB', 'SG'],
+    #     'D': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'OD1', 'OD2'],
+    #     'E': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD', 'OE1', 'OE2'],
+    #     'F': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD1', 'CD2', 'CE1', 'CE2', 'CZ'],
+    #     'G': ['N', 'CA', 'C', 'O'],
+    #     'H': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'ND1', 'CD2', 'CE1', 'NE2'],
+    #     'I': ['N', 'CA', 'C', 'O', 'CB', 'CG1', 'CG2', 'CD1'],
+    #     'K': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD', 'CE', 'NZ'],
+    #     'L': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD1', 'CD2'],
+    #     'M': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'SD', 'CE'],
+    #     'N': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'OD1', 'ND2'],
+    #     'P': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD'],
+    #     'Q': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD', 'OE1', 'NE2'],
+    #     'R': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD', 'NE', 'CZ', 'NH1', 'NH2'],
+    #     'S': ['N', 'CA', 'C', 'O', 'CB', 'OG'],
+    #     'T': ['N', 'CA', 'C', 'O', 'CB', 'OG1', 'CG2'],
+    #     'V': ['N', 'CA', 'C', 'O', 'CB', 'CG1', 'CG2'],
+    #     'W': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD1', 'CD2', 'NE1', 'CE2', 'CE3', 'CZ2', 'CZ3', 'CH2'],
+    #     'Y': ['N', 'CA', 'C', 'O', 'CB', 'CG', 'CD1', 'CD2', 'CE1', 'CE2', 'CZ', 'OH']
+    # }
+    # with open('../data/jsons/aa_atoms.json', 'w') as json_f:
+    #     json.dump(aa_atoms, json_f, indent=4)
 
-    # Translate to atomic sequence
-    atomic_sequence = translate_to_atoms(amino_acid_sequence)
+    aa_atom_path = '../data/jsons/aa_atoms.json'
+    try:
+        with open(aa_atom_path, 'r') as json_f:
+            amino_acid_atoms = json.load(json_f)
+    except FileNotFoundError:
+        print(f'{aa_atom_path} does not exist.')
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    # Print the atomic sequence
-    print(atomic_sequence)
+    fasta_sequences = reader.read_fasta_sequences()
+
+    atomic_sequences = []
+    for fasta_sequence in fasta_sequences:
+        atomic_seq = translate_to_atoms(aa_fasta_sequence=fasta_sequence)
+        atomic_sequences.append(atomic_seq)
+    print(atomic_sequences)
--- src/fasta_reader.py
+++ src/fasta_reader.py
@@ -1,30 +1,89 @@
 """
-Read local fasta sequence files of proteins of interest, downloaded from UniProt.
+Read local FASTA sequence files of proteins of interest, downloaded from UniProt.
 """
-
-from Bio import SeqIO
+import os
+import glob
 from enum import Enum
+from Bio import SeqIO
+import requests
+from io import StringIO
 
 
-class Sequences(Enum):
+class PathsUrls(Enum):
+    fastas_path = '../data/FASTA'
+    Uniprot_API = 'https://www.uniprot.org/uniprot/'
+
+
+# Unique identifiers for proteins
+class UniprotId(Enum):
+    Myoglobin_Human = 'P02144'
     Myoglobin_Sperm_whale = 'P02185'
-    Serine_Protease_Human = 'PRS57'
-    Low_resolution_structure = ''  # resolution > 4 angstroms
+    Serine_Protease_Human = 'Q6UWY2'
+    Low_resolution_structure = ''  # resolution > 4 angstroms ??
     Cyclic_peptide = ''
     NMR_structure = ''  # for fun ! (you'd need to skip all the Hydrogens)
 
 
-def read_fasta_sequences():
-    myoglob_fasta = SeqIO.read(f'../data/fastas/{Sequences.Myoglobin_Sperm_whale.value}.faa', format='fasta')
-    print(f'Sperm whale Myoglobin sequence: {str(myoglob_fasta.seq)}'
-          f'\nLength = {len(str(myoglob_fasta.seq))} amino acids')
+def read_fasta_sequences(uniprot_ids=None) -> dict:
+    """
+    Read specified FASTA sequences in.
+    If FASTA file of id is not found locally in `../data/FASTA/`, an attempt will be made to read it directly from
+    Uniprot API.
+    If no identifier passed in, all FASTA files will be read from `../data/FASTA/`.
+    :param uniprot_ids: A list of unique Uniprot identifiers.
+    :return: 1-letter FASTA amino acid sequence(s) mapped to the identifier(s).
+    """
+    if isinstance(uniprot_ids, str):
+        uniprot_ids = [uniprot_ids]
 
-    serine_protease_fasta = SeqIO.read(f'../data/fastas/{Sequences.Serine_Protease_Human.value}.faa', format='fasta')
-    print(f'Human Serine Protease sequence: {str(serine_protease_fasta.seq)}'
-          f'\nLength = {len(str(serine_protease_fasta.seq))} amino acids')
+    fasta_id_seqs = {}
 
-    return myoglob_fasta, serine_protease_fasta
+    if not uniprot_ids:
+        uniprot_ids = []
+        print(f"No FASTA files were specified, "
+              f"so all FASTA files found at '{PathsUrls.fastas_path.value}' will be read in.")
+        fasta_files = glob.glob(os.path.join(PathsUrls.fastas_path.value, '*'))
+        for fasta_file in fasta_files:
+            fasta_id_faa = os.path.basename(fasta_file)
+            fasta_id = os.path.splitext(fasta_id_faa)[0]
+            uniprot_ids.append(fasta_id)
+
+    for fasta_id in uniprot_ids:
+
+        fasta_path = os.path.join(PathsUrls.fastas_path.value, fasta_id)
+        fasta_path_faa = f'{fasta_path}.faa'
+
+        try:
+            record = SeqIO.read(fasta_path_faa, format='fasta')
+            fasta_id_seqs[fasta_id] = str(record.seq)
+
+        except FileNotFoundError:
+
+            print(f"File '{fasta_path_faa}' not found")
+
+            print(f'Will try to read {fasta_id} directly from Uniprot site..')
+            try:
+                url = f'{PathsUrls.Uniprot_API.value}{fasta_id}.fasta'
+                response = requests.get(url)
+                response.raise_for_status()
+                fasta_data = StringIO(response.text)
+                record = SeqIO.read(fasta_data, 'fasta')
+                fasta_id_seqs[fasta_id] = str(record.seq)
+
+            except requests.exceptions.RequestException as e:
+                print(f'Failed to retrieve data from API: {e}')
+
+            except Exception:
+                print(f"Undefined error while trying to read in {fasta_path_faa}'.")
+
+        except Exception:
+            print(f"Undefined error while trying to read in {fasta_path_faa}'.")
+
+    return fasta_id_seqs
 
 
 if __name__ == '__main__':
-    myoglob, ser_prot = read_fasta_sequences()
+
+    fasta_id_seqs_ = read_fasta_sequences(uniprot_ids=UniprotId.Serine_Protease_Human.value)
+    print(fasta_id_seqs_)
+
