064d9764b7de4767c3a42beabaede0f5c8826d42
shahin <shahin.zibaee.23@ucl.ac.uk>
2024-07-05 18:20:09 +0100

    Update `tokeniser.py` to replace A_Cartn_x, y, z with the mean-corrected coords. Write out to csv, ssv, tsv. (Remove previous csv, ssv, tsv with '_cif.csv' suffix).

--- src/tokeniser.py
+++ src/tokeniser.py
@@ -25,16 +25,19 @@ def _read_enumeration_mappings():
 
 
 def _write_to_csv(pdb_id: str, pdf: pd.DataFrame):
-    pdf.to_csv(path_or_buf=f'../data/tokenised/{pdb_id}.csv', index=False, na_rep='null')
-    pdf.to_csv(path_or_buf=f'../data/tokenised/{pdb_id}.ssv', sep=' ', index=False, na_rep='null')  # space-separated
-    pdf.to_csv(path_or_buf=f'../data/tokenised/{pdb_id}.tsv', sep='\t', index=False, na_rep='null')  # tab-separated
+    # pdf.to_csv(path_or_buf=f'../data/tokenised/{pdb_id}.csv', index=False, na_rep='null')
+    pdf.to_csv(path_or_buf=f'../data/tokenised/{pdb_id}.csv', index=False)
+    # pdf.to_csv(path_or_buf=f'../data/tokenised/{pdb_id}.ssv', sep=' ', index=False, na_rep='null')
+    pdf.to_csv(path_or_buf=f'../data/tokenised/{pdb_id}.ssv', sep=' ', index=False)
+    # pdf.to_csv(path_or_buf=f'../data/tokenised/{pdb_id}.tsv', sep='\t', index=False, na_rep='null')
+    pdf.to_csv(path_or_buf=f'../data/tokenised/{pdb_id}.tsv', sep='\t', index=False)
     pdf_easy_read = pdf.rename(columns={CIF.S_seq_id.value: 'SEQ_ID',
                                         CIF.S_mon_id.value: 'RESIDUES',
                                         CIF.A_id.value: 'ATOM_ID',
                                         CIF.A_label_atom_id.value: 'ATOMS',
-                                        CIF.A_Cartn_x.value: 'X',
-                                        CIF.A_Cartn_y.value: 'Y',
-                                        CIF.A_Cartn_z.value: 'Z'})
+                                        ColNames.MEAN_CORR_X.value: 'X',
+                                        ColNames.MEAN_CORR_Y.value: 'Y',
+                                        ColNames.MEAN_CORR_Z.value: 'Z'})
     pdf_easy_read.to_csv(path_or_buf=f'../data/tokenised/easyRead_{pdb_id}.tsv', sep='\t', index=False, na_rep='null')
 
 
@@ -51,21 +54,11 @@ def write_tokenised_cif_to_csv(pdb_ids=None) -> None:
         pdf_cif = parser.parse_cif(pdb_id=pdb_id, local_cif_file=f'../data/cifs/{pdb_id}.cif')
         atoms_enumerated, aas_enumerated = _read_enumeration_mappings()
 
-        # # Amino acid indices
-        # aa_index = np.asarray(pdf_cif[CIF.S_seq_id.value].tolist(), dtype=np.uint16)
-        # # Atom indices
-        # pdf_cif[CIF.A_id.value].fillna(0, inplace=True)
-        # atom_index = np.asarray(pdf_cif[CIF.A_id.value].tolist(), dtype=np.uint16)
-
         # Amino acid labels enumerated
         pdf_cif[ColNames.AA_LABEL_NUM.value] = pdf_cif[CIF.S_mon_id.value].map(aas_enumerated).astype('Int64')
-        pdf_cif[ColNames.AA_LABEL_NUM.value].fillna(255, inplace=True)
-        pdf_cif[ColNames.AA_LABEL_NUM.value] = pdf_cif[ColNames.AA_LABEL_NUM.value].astype('uint8')
 
         # Atom labels enumerated
         pdf_cif[ColNames.ATOM_LABEL_NUM.value] = pdf_cif[CIF.A_label_atom_id.value].map(atoms_enumerated).astype('Int64')
-        pdf_cif[ColNames.ATOM_LABEL_NUM.value].fillna(255, inplace=True)
-        pdf_cif[ColNames.ATOM_LABEL_NUM.value] = pdf_cif[ColNames.ATOM_LABEL_NUM.value].astype('uint8')
 
         # Atomic xyz coordinates
         pdf_cif[ColNames.MEAN_COORDS.value] = pdf_cif[[CIF.A_Cartn_x.value,
@@ -75,10 +68,14 @@ def write_tokenised_cif_to_csv(pdb_ids=None) -> None:
         pdf_cif[ColNames.MEAN_CORR_Y.value] = pdf_cif[CIF.A_Cartn_y.value] - pdf_cif[ColNames.MEAN_COORDS.value]
         pdf_cif[ColNames.MEAN_CORR_Z.value] = pdf_cif[CIF.A_Cartn_z.value] - pdf_cif[ColNames.MEAN_COORDS.value]
 
-        # alpha_carbon_indices = np.where(pdf_cif[CIF.A_label_atom_id.value] == 'CA',
-        #                                 pdf_cif[CIF.A_id.value], np.nan)
-        # alpha_carbon_indices = alpha_carbon_indices[~np.isnan(alpha_carbon_indices)]
-        # alpha_carbon_indices = np.asarray(alpha_carbon_indices, dtype=np.uint16)
+        # ONLY KEEP THESE COLUMNS:
+        pdf_cif = pdf_cif[[CIF.S_seq_id.value,
+                           CIF.S_mon_id.value,
+                           CIF.A_id.value,
+                           CIF.A_label_atom_id.value,
+                           ColNames.MEAN_CORR_X.value,
+                           ColNames.MEAN_CORR_Y.value,
+                           ColNames.MEAN_CORR_Z.value]]
         _write_to_csv(pdb_id, pdf_cif)
 
 
