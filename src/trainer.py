7df0507b855f70801a681dbbefbb0b2da3b57c20
shahin <shahin.zibaee.23@ucl.ac.uk>
2024-07-08 18:29:14 +0100

    Complete and run trainer on the 573 single-domain cifs. (Execution halted at one cif which lacked '_pdbx_poly_seq_scheme.seq_id'. I'll need to find out which it is, how common it is the case and refactor trainer.py or delete cifs that lack this.)

--- src/trainer.py
+++ src/trainer.py
@@ -1,10 +1,12 @@
+import os
+import glob
 from enum import Enum
 import tokeniser as tk
 
 # TRAINING: input is amino acid sequence --> output should be xyz coords for atoms that are seen, else nan.
 
 
-class PDBid(Enum):
+class PDBidParticularProteins(Enum):
     Myoglobin_pig = '1mwd'  # WT deoxy myoglobin
     Serine_Protease_Human = '4Q7Z' # 'Q6UWY2' X-ray 1.40 Ang aa34-283
     Low_resolution_structure = ''  # resolution > 4 angstroms ??
@@ -13,7 +15,14 @@ class PDBid(Enum):
 
 
 if __name__ == '__main__':
+    cifs = glob.glob(os.path.join('../data/cifs', '*.cif'))
+    path_cifs = [cif for cif in cifs if os.path.isfile(cif)]
 
-    # tk.write_tokenised_cif_to_csv(pdb_ids=['1MWD', '4hb1'])
-    tk.write_tokenised_cif_to_csv(pdb_ids=['4hb1'])
+    pdb_ids = []
+    for path_cif in path_cifs:
+        cif = os.path.basename(path_cif)
+        cif = os.path.splitext(cif)[0]
+        pdb_ids.append(cif.rstrip('.cif'))
+
+    tk.write_tokenised_cif_to_csv(pdb_ids)
 
