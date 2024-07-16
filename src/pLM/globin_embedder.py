0f2a3095034ef28ce44b0b2be096e0130e2403d0
shahin <shahin.zibaee.23@ucl.ac.uk>
2024-07-16 19:30:41 +0100

    Update to exclude null values from dict of fastas.

--- src/pLM/globin_embedder.py
+++ src/pLM/globin_embedder.py
@@ -3,7 +3,7 @@ import plm_embedder as pe
 
 
 def generate_ankh_base_embeddings_of_5_globins_from_fastas_of_pdbids():
-    globins_pdb_fastas = dh.read_fastas_from_json_to_dict(filename='pdbids_sp_fastas_globins_5')
+    globins_pdb_fastas = dh.read_nonnull_fastas_from_json_to_dict(filename='pdbids_sp_fastas_globins_5')
     tokeniser, eval_model = pe.load_tokeniser_and_eval_model(model_name=pe.HFModelName.ANKH_BASE.value)
     globins_pdbid_raw_tok_emb = pe.generate_embeddings_from_fastas_of_pdbids(tokeniser=tokeniser,
                                                                              eval_model=eval_model,
