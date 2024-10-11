39aca00dfa9496f2445cd427998b1446e6721f27
shahin <shahin.zibaee.23@ucl.ac.uk>
2024-10-11 16:07:23 +0100

    Add test files called from test qsub script.

---
+++ data_layer/hellodependency.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def somefunc(ary: np.ndarray):
+    print(f'hello from data_layer.hellodependency.py somefunc: passed np array ={ary}')
---
+++ rnatrain/hello_from_rnatrain.py
@@ -0,0 +1,7 @@
+from data_layer import hellodependency
+import numpy as np
+
+
+if __name__ == '__main__':
+    ary = np.random.rand(1, 3)
+    hellodependency.somefunc(ary)
