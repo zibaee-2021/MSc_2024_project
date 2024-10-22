9e93934b0943623698557b2f4dc8632cc76744d0
shahin <shahin.zibaee.23@ucl.ac.uk>
2024-10-22 19:08:03 +0100

    Move colnames enums into Tokeniser. Delete enums dir.

--- src/general_utility_methods/tokeniser.py
+++ src/general_utility_methods/tokeniser.py
@@ -4,7 +4,17 @@ import pandas as pd
 from src.general_utility_methods import cif_parser as parser
 from src.general_utility_methods.cif_parser import CIF
 from data_layer import data_handler as dh
-from enums.colnames import ColNames
+from enum import Enum
+
+
+class ColNames(Enum):
+    AA_LABEL_NUM = 'aa_label_num'  # `A_label_comp_id` enumerated (the amino acid)
+    ATOM_LABEL_NUM = 'atom_label_num'  # `A_label_atom_id` enumerated (the atom)
+    BB_INDEX = 'bb_index'  # NOT 100% CERTAIN EXACTLY WHAT THIS IS .. BACKBONE ATOMS .. WHICH PART OF BB ?
+    MEAN_COORDS = 'mean_xyz'  # mean of x y z coordinates for each atom
+    MEAN_CORR_X = 'mean_corrected_x'  # x coordinates for each atom subtracted by the mean of xyz coordinates
+    MEAN_CORR_Y = 'mean_corrected_y'  # (as above) but for y coordinates
+    MEAN_CORR_Z = 'mean_corrected_z'  # (as above) but for z coordinates
 
 
 def parse_tokenise_cif_write_to_flatfile_to_pdf(pdb_ids=None, use_local_data_subdir=False, flatfile: str = 'ssv') -> pd.DataFrame:
@@ -21,10 +31,10 @@ def parse_tokenise_cif_write_to_flatfile_to_pdf(pdb_ids=None, use_local_data_sub
         pdb_ids = [pdb_ids]
 
     for pdb_id in pdb_ids:
-        # os.getcwd() gives 'diffSock/src/diffusion'
-        path_to_cif_pdb_ids = f'{os.getcwd()}/data/cif/'
-        assert os.path.exists(path_to_cif_pdb_ids)
-        pdf_cif = parser.parse_cif(pdb_id=pdb_id, local_cif_file=f'data/cif/{pdb_id}.cif')
+        path_to_cif_pdb_ids = f'../diffusion/data/cif/'
+        cif = f'{path_to_cif_pdb_ids}{pdb_id}.cif'
+        assert os.path.exists(cif)
+        pdf_cif = parser.parse_cif(pdb_id=pdb_id, local_cif_file=cif)
 
         atoms_enumerated, aas_enumerated, fasta_aas_enumerated = dh.read_enumeration_mappings()
 
@@ -47,9 +57,11 @@ def parse_tokenise_cif_write_to_flatfile_to_pdf(pdb_ids=None, use_local_data_sub
 
         # ONLY KEEP THESE COLUMNS:
         pdf_cif = pdf_cif[[CIF.S_seq_id.value,
-                           CIF.S_mon_id.value,
+                           CIF.S_mon_id.value,  # temporarily keeping this col but mapped to enumerated form below
+                           ColNames.AA_LABEL_NUM.value,  # to replace S_mon_id, but check first it gives what you expect before dropping S_mon_id
                            CIF.A_id.value,
-                           CIF.A_label_atom_id.value,
+                           CIF.A_label_atom_id.value,  # temporarily keeping this col but mapped to enumerated form below
+                           ColNames.ATOM_LABEL_NUM.value,  # to replace A_label_atom_id, but check first it gives what you expect before dropping A_label_atom_id
                            ColNames.MEAN_CORR_X.value,
                            ColNames.MEAN_CORR_Y.value,
                            ColNames.MEAN_CORR_Z.value]]
@@ -62,4 +74,5 @@ def parse_tokenise_cif_write_to_flatfile_to_pdf(pdb_ids=None, use_local_data_sub
 if __name__ == '__main__':
 
     # write_tokenised_cif_to_csv(pdb_ids='4itq')
-    parse_tokenise_cif_write_to_flatfile_to_pdf(pdb_ids='1oj6')
+    parse_tokenise_cif_write_to_flatfile_to_pdf(pdb_ids='1OJ6')
+    pass
