ff6688689cd723e391b613e569efa497e8de798e
shahin <shahin.zibaee.23@ucl.ac.uk>
2024-10-25 20:27:52 +0100

    Comments, docstring for module.

--- src/diffusion/pytorch_protfold_allatomclustrain_singlegpu.py
+++ src/diffusion/pytorch_protfold_allatomclustrain_singlegpu.py
@@ -1,4 +1,6 @@
 #!/home/jones/miniconda3/bin/python
+"""
+DJ's diffusion method for training and inference of RNA structures from primary sequence, adapted here for proteins.
 
 General notes:
 - `pdf_` refers to pandas dataframe.
