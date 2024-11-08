5b0182361a90c469c6de04a6945f55783606bb67
shahin <shahin.zibaee.23@ucl.ac.uk>
2024-11-08 18:17:40 +0000

    Generate data for unittest and assign re-used path strings to class in setUp(), moving some code from test.

--- test/test_preprocessing_funcs.py
+++ test/test_preprocessing_funcs.py
@@ -1,7 +1,9 @@
+import os, sys
 from unittest import TestCase
 from src.preprocessing_funcs import tokeniser as tk
 from src.preprocessing_funcs import cif_parser
 import pandas as pd
+from Bio.PDB.MMCIF2Dict import MMCIF2Dict
 """
 Expected 17 columns of output dataframe of `parse_tokenise_cif_write_flatfile()`:
 
@@ -27,6 +29,24 @@ mean_corrected_z      # Z-COORDINATES FOR EACH ATOM SUBTRACTED BY THE MEAN OF XY
 
 class TestPreprocessingFuncs(TestCase):
 
+    def setUp(self):
+        self.cif_dir = 'test_data/cif'
+        self.tokenised_dir = 'test_data/tokenised'
+        self.pdf_merged_dir = 'test_data/merged'
+        self.test_1V5H_ssv = 'test_1V5H.ssv'
+        self.test_1V5H_cif = 'test_1V5H.cif'
+
+        not_yet_called = False
+        # CALL ONLY ONCE (i.e. SET FLAG TO FALSE AFTER CALL)
+        if not_yet_called:
+            mmcif_dict = MMCIF2Dict(f'{self.cif_dir}/{self.test_1V5H_cif}')
+            polyseq_pdf = cif_parser._extract_fields_from_poly_seq(mmcif_dict)
+            atomsite_pdf = cif_parser._extract_fields_from_atom_site(mmcif_dict)
+            atomsite_pdf = cif_parser._remove_hetatm_rows(atomsite_pdf)
+            pdf_merged = cif_parser._join_atomsite_to_polyseq(atomsite_pdf, polyseq_pdf)
+            os.makedirs(self.pdf_merged_dir, exist_ok=True)
+            pdf_merged.to_csv(path_or_buf=f'{self.pdf_merged_dir}/{self.test_1V5H_ssv}', sep=' ', index=False)
+
     def test_parse_tokenise_cif_write_flatfile(self):
 
         pdf = tk.parse_tokenise_cif_write_flatfile(pdb_ids='test_1V5H',
@@ -37,24 +57,20 @@ class TestPreprocessingFuncs(TestCase):
         self.assertEqual(18, len(pdf.columns))
 
     def test_datatypes_after_casting(self):
-        mmcif_dict = cif_parser._get_mmcif_data(pdb_id='test_1V5H', relpath_to_raw_cif='test_data/cif')
-        polyseq_pdf = cif_parser._extract_fields_from_poly_seq(mmcif_dict)
-        atomsite_pdf = cif_parser._extract_fields_from_atom_site(mmcif_dict)
-        atomsite_pdf = cif_parser._remove_hetatm_rows(atomsite_pdf)
-        pdf_merged = cif_parser._join_atomsite_to_polyseq(atomsite_pdf, polyseq_pdf)
-        pdf_merged = cif_parser._cast_objects_to_stringdtype(pdf_merged)
+        pdf_merged = pd.read_csv(f'{self.pdf_merged_dir}/{self.test_1V5H_ssv}', sep=' ')
         pdf_merged = cif_parser._cast_number_strings_to_numeric_types(pdf_merged)
+        pdf_merged = cif_parser._cast_objects_to_stringdtype(pdf_merged)
         expected_int64_dtype = pd.Int64Dtype()
-        self.assertEqual(pdf_merged['S_seq_id'].dtype , expected_int64_dtype)
-        self.assertEqual(pdf_merged['A_label_seq_id'].dtype , expected_int64_dtype)
-        self.assertEqual(pdf_merged['S_pdb_seq_num'].dtype , expected_int64_dtype)
-        self.assertEqual(pdf_merged['A_id'].dtype , expected_int64_dtype)
-        self.assertEqual(pdf_merged['S_mon_id'].dtype , 'string')
-        self.assertEqual(pdf_merged['A_label_comp_id'].dtype , 'string')
-        self.assertEqual(pdf_merged['A_label_asym_id'].dtype , 'string')
-        self.assertEqual(pdf_merged['S_asym_id'].dtype , 'string')
-        self.assertEqual(pdf_merged['A_Cartn_x'].dtype , 'float64')
-        self.assertEqual(pdf_merged['A_Cartn_y'].dtype , 'float64')
-        self.assertEqual(pdf_merged['A_Cartn_z'].dtype , 'float64')
-        self.assertEqual(pdf_merged['A_occupancy'].dtype , 'float64')
+        self.assertEqual(pdf_merged['S_seq_id'].dtype, expected_int64_dtype)
+        self.assertEqual(pdf_merged['A_label_seq_id'].dtype, expected_int64_dtype)
+        self.assertEqual(pdf_merged['S_pdb_seq_num'].dtype, expected_int64_dtype)
+        self.assertEqual(pdf_merged['A_id'].dtype, expected_int64_dtype)
+        self.assertEqual(pdf_merged['S_mon_id'].dtype, 'string')
+        self.assertEqual(pdf_merged['A_label_comp_id'].dtype, 'string')
+        self.assertEqual(pdf_merged['A_label_asym_id'].dtype, 'string')
+        self.assertEqual(pdf_merged['S_asym_id'].dtype, 'string')
+        self.assertEqual(pdf_merged['A_Cartn_x'].dtype, 'float64')
+        self.assertEqual(pdf_merged['A_Cartn_y'].dtype, 'float64')
+        self.assertEqual(pdf_merged['A_Cartn_z'].dtype, 'float64')
+        self.assertEqual(pdf_merged['A_occupancy'].dtype, 'float64')
 
