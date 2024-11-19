1d719a73f22fc071b48aeefc5f3ba7ce7f005761
shahin <shahin.zibaee.23@ucl.ac.uk>
2024-11-19 15:28:01 +0000

    (Comment out code that is not needed to run more than once. Note: this was run in qrsh anyway. Just leaving this here as documentation.)

--- bash_for_hpc/shahintest_rnatrain.qsub.sh
+++ bash_for_hpc/shahintest_rnatrain.qsub.sh
@@ -22,6 +22,10 @@ echo 'which python: '; which python
 echo 'python --version:'; python --version
 
 conda activate shahin_msc24
+
+## Only need to run this command once to install. In fact, I ran this via qrsh (after running conda activate shahin_msc).
+#conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+
 conda list | grep -i "cuda\|cudnn\|torch"
 #!/usr/bin/env bash
 #source ~/.bashrc
@@ -31,8 +35,8 @@ conda list | grep -i "cuda\|cudnn\|torch"
 #export PYTHONPATH=$PYTHONPATH:~/miniconda3/envs/shahin_msc24/lib/python3.12/site-packages
 #export LD_LIBRARY_PATH=~/miniconda3/envs/shahin_msc24/lib:$LD_LIBRARY_PATH
 
-# Set the CUDA_LAUNCH_BLOCKING environment variable for debugging
-export CUDA_LAUNCH_BLOCKING=1
+## Set the CUDA_LAUNCH_BLOCKING environment variable for debugging
+#export CUDA_LAUNCH_BLOCKING=1
 
 cd ~/diffSock_HPC/rnatrain_DJ_code || exit
 python pytorch_rnafold_allatomclustrain_singlegpu.py
