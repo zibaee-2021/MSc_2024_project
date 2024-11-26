e773500abe5de94fc48dcc1a319f193f0ecdcba1
zibaee-2021 <shahinjzibaee@hotmail.co.uk>
2024-11-26 16:46:55 +0000

    Include relative path.

--- src/trainer.py
+++ src/trainer.py
@@ -11,8 +11,8 @@ from src.preprocessing_funcs import tokeniser as tk
 from data_layer import data_handler as dh
 
 
-class Paths(Enum):
-    pdbid_dir = 'PDBid'
+class Path(Enum):
+    pdbid_dir = 'diffusion/diff_data/PDBid_list'
 
 
 # ONLY NEED TO RUN THESE ONCE FOR A PARTICULAR DATASET
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     SD_PDBids = dh.get_list_of_pdbids_of_local_single_domain_cifs()
     cif_count = len(SD_PDBids)
     dh.write_list_to_lst_file(list_to_write=SD_PDBids,
-                              fname=f'{Paths.pdbid_dir.value}/SD_{cif_count}.lst')
+                              path_fname=f'{Path.pdbid_dir.value}/SD_{cif_count}.lst')
     # dh.write_list_to_space_separated_txt_file(list_to_write=SD_PDBids,
     #                                           fname=f'{Paths.pdbid_dir.value}/SD_{cif_count}.txt')
 
