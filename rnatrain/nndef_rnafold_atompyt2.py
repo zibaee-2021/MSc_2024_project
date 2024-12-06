5ae42d25c43a35dcf9d7884f5c29f7ac8db40abc
shahin <shahin.zibaee.23@ucl.ac.uk>
2024-12-06 18:50:54 +0000

    (stylistic edit for readability)

--- rnatrain/nndef_rnafold_atompyt2.py
+++ rnatrain/nndef_rnafold_atompyt2.py
@@ -283,7 +283,12 @@ class DiffusionNet(nn.Module):
 
         coordscale = (nlev_in.view(-1,1,1).pow(2) + VARDATA).sqrt()
         
-        atom_x = atom_x + self.nt_embed(ntcodes)[ntindices].unsqueeze(0) + self.atom_embed(atcodes).unsqueeze(0) + self.ntidx_embed(ntindices).unsqueeze(0) + self.nlev_embed(nlev_in).unsqueeze(1) + self.coord_embed(noised_coords_in / coordscale)
+        atom_x = (atom_x
+                  + self.nt_embed(ntcodes)[ntindices].unsqueeze(0)
+                  + self.atom_embed(atcodes).unsqueeze(0)
+                  + self.ntidx_embed(ntindices).unsqueeze(0)
+                  + self.nlev_embed(nlev_in).unsqueeze(1)
+                  + self.coord_embed(noised_coords_in / coordscale))
 
         atom_x = self.norm3(atom_x)
 
