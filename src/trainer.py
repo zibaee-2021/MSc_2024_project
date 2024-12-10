d84b151ada8af210ee78d7552eafe76acd372e47
shahin <shahin.zibaee.23@ucl.ac.uk>
2024-12-10 13:54:36 +0000

    Remove data preparation steps from trainer.py. (To be completed, but may be unnecessary extra way to essentially just call `pytorch_protfold_allatomclustrain_singlegpu.py.main()`.)

--- src/trainer.py
+++ src/trainer.py
@@ -14,17 +14,7 @@ from data_layer import data_handler as dh
 # class Path(Enum):
 #     pdbid_dir = 'diffusion/diff_data/PDBid_list'
 
-
+# TODO
 # ONLY NEED TO RUN THESE ONCE FOR A PARTICULAR DATASET
-if __name__ == '__main__':
-
-    SD_PDBids = dh.get_list_of_pdbids_of_local_single_domain_cifs()
-    cif_count = len(SD_PDBids)
-    dh.write_list_to_lst_file(list_to_write=SD_PDBids,
-                              # path_fname=f'{Path.pdbid_dir.value}/SD_{cif_count}.lst')
-                              path_fname=f'diffusion/diff_data/PDBid_list/SD_{cif_count}.lst')
-    # dh.write_list_to_space_separated_txt_file(list_to_write=SD_PDBids,
-    #                                           fname=f'{Paths.pdbid_dir.value}/SD_{cif_count}.txt')
-
-    # tk.write_tokenised_cif_to_csv(pdb_ids)
-
+# if __name__ == '__main__':
+#     pass
