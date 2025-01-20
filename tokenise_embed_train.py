27dd68ce279789f862a5fa14d02689ecdb7aab69
zibaee-2021 <shahinjzibaee@hotmail.co.uk>
2025-01-20 21:58:56 +0000

    Remove unused targetfile_lst_path.

--- tokenise_embed_train.py
+++ tokenise_embed_train.py
@@ -10,15 +10,12 @@ if __name__ == "__main__":
 
     # TOKENISE:
     data_handler.clear_diffdata_tokenised_dir()
-    _, lst_file = tokeniser.parse_tokenise_write_cifs_to_flatfile()
+    tokeniser.parse_tokenise_write_cifs_to_flatfile()
 
     # MAKE pLM EMBEDDINGS:
     data_handler.clear_diffdata_emb_dir()
     plm_embedder.generate_ankh_base_embeddings_from_tokenised_cifs()
 
     # TRAIN MODEL:
-    _abs_path = os.path.dirname(os.path.abspath(__file__))
-    _targetfile_lst_path = os.path.normpath(os.path.join(_abs_path, f'diff_data/PDBid_list/{lst_file}'))
-    assert os.path.exists(_targetfile_lst_path), f'{_targetfile_lst_path} cannot be found. Forget about it.'
-    pytorch_protfold_allatomclustrain_singlegpu.main(_targetfile_lst_path)
+    pytorch_protfold_allatomclustrain_singlegpu.main()
 
