a52bddbf6d94cff90def2562a7e349b381c611cd
zibaee-2021 <shahinjzibaee@hotmail.co.uk>
2025-01-21 22:06:41 +0000

    Pass and take pdblist argument at cmd line

--- launchers/tokenise_embed_train.sh
+++ launchers/tokenise_embed_train.sh
@@ -8,4 +8,4 @@ export PYTHONPATH=$PYTHONPATH:$HOME/diffSock/src
 export PYTHONPATH=$PYTHONPATH:$HOME/diffSock/src/diffusion
 
 #!/usr/bin/env python3
-python3 ~/PycharmProjects/MSc_project/diffSock/src/diffusion/tokenise_embed_train.py
+python3 ~/PycharmProjects/MSc_project/diffSock/src/diffusion/tokenise_embed_train.py pdbchains_565
--- src/diffusion/tokenise_embed_train.py
+++ src/diffusion/tokenise_embed_train.py
@@ -1,4 +1,6 @@
 #!~/miniconda3/bin/python
+import os
+import sys
 from data_layer import data_handler
 from src.preprocessing_funcs import tokeniser
 from src.pLM import plm_embedder
@@ -10,7 +12,17 @@ if __name__ == "__main__":
     # TOKENISE:
     data_handler.clear_diffdata_mmcif_dir()
     data_handler.clear_diffdata_tokenised_dir()
-    tokeniser.parse_tokenise_write_cifs_to_flatfile()
+
+    if len(sys.argv) > 1:
+        relpath_pdblst = f'../diffusion/diff_data//PDBid_list/{sys.argv[1]}.lst'
+        print(f'Argument received --> will use this list of PDBids: {relpath_pdblst}')
+
+        # 5. PARSE AND TOKENISE MMCIF FILES AND WRITE SSV FILES TO `DIFFUSION/DIFF_DATA/TOKENISED` DIRECTORY:
+        tokeniser.parse_tokenise_write_cifs_to_flatfile(relpath_pdblst=relpath_pdblst)
+    else:
+        print('No PDBid list filename argument provided.')
+        # 5. PARSE AND TOKENISE MMCIF FILES AND WRITE SSV FILES TO `DIFFUSION/DIFF_DATA/TOKENISED` DIRECTORY:
+        tokeniser.parse_tokenise_write_cifs_to_flatfile()
 
     # MAKE pLM EMBEDDINGS:
     data_handler.clear_diffdata_emb_dir()
