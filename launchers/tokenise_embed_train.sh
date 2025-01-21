dd4655ed371ef1311f20ed2c8581d591b7c4c376
zibaee-2021 <shahinjzibaee@hotmail.co.uk>
2025-01-21 00:02:54 +0000

    Add new shell

---
+++ launchers/tokenise_embed_train.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+source ~/.bashrc
+source ~/miniconda3/bin/activate diffSock
+
+export PYTHONPATH=$PYTHONPATH:$HOME/diffSock
+export PYTHONPATH=$PYTHONPATH:$HOME/diffSock/data_layer
+
+#!/usr/bin/env python3
+python3 ~/PycharmProjects/MSc_project/diffSock/tokenise_embed_train.py
