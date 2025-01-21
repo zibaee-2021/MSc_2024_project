d7e2c7f6607df24461f76b22ae893070b5eef242
zibaee-2021 <shahinjzibaee@hotmail.co.uk>
2025-01-21 01:09:38 +0000

    Add path.

--- launchers/tokenise_embed_train.sh
+++ launchers/tokenise_embed_train.sh
@@ -5,6 +5,7 @@ source ~/miniconda3/bin/activate diffSock
 export PYTHONPATH=$PYTHONPATH:$HOME/diffSock
 export PYTHONPATH=$PYTHONPATH:$HOME/diffSock/data_layer
 export PYTHONPATH=$PYTHONPATH:$HOME/diffSock/src
+export PYTHONPATH=$PYTHONPATH:$HOME/diffSock/src/diffusion
 
 #!/usr/bin/env python3
 python3 ~/PycharmProjects/MSc_project/diffSock/tokenise_embed_train.py
